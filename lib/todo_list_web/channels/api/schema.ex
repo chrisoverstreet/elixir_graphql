d66b0c27bdf4a0a8f8748e0deb3233b3ae9d563b
Christopher Overstreet <christopheroverstreet@gmail.com>
2021-06-21 16:57:35 -0400

    Add createTodoItem mutation

--- lib/todo_list_web/channels/api/schema.ex
+++ lib/todo_list_web/channels/api/schema.ex
@@ -1,12 +1,36 @@
 defmodule TodoListWeb.Api.Schema do
   use Absinthe.Schema
 
+  alias TodoList.Todos
+
   object :todo_item do
     field :id, non_null(:id) # ID!
     field :content, non_null(:string) # String!
     field :is_completed, non_null(:boolean) do
       resolve(fn %{completed_at: completed_at}, _, _ ->
         {:ok, !is_nil(completed_at)}
+      end)
+    end
+  end
+
+  mutation do
+    field :create_todo_item, non_null(:boolean) do
+      arg :content, non_null(:string)
+
+      resolve fn %{content: content}, _ ->
+        case Todos.create_item(%{content: content }) do
+          {:ok, %Todos.Item{}} ->
+            {:ok, true}
+          _ ->
+            {:ok, false}
+        end
+      end
+    end
+
+    field :toggle_todo_item, :todo_item do
+      arg(:id, non_null(:id))
+      resolve(fn %{id: item_id}, _ ->
+        Todos.toggle_todo_item_by_id(item_id)
       end)
     end
   end
@@ -20,7 +44,7 @@ defmodule TodoListWeb.Api.Schema do
 
     field :todo_items, non_null(list_of(:todo_item)) do # [TodoItem!]!
       resolve fn _, _ ->
-        {:ok, TodoList.Todos.list_items()}
+        {:ok, Todos.list_items()}
       end
     end
   end
