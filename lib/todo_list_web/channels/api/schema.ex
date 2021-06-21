7e24c0c91df325e66aa61305cfbf7a57b0297bba
Christopher Overstreet <christopheroverstreet@gmail.com>
2021-06-21 16:36:32 -0400

    Init

---
+++ lib/todo_list_web/channels/api/schema.ex
@@ -0,0 +1,27 @@
+defmodule TodoListWeb.Api.Schema do
+  use Absinthe.Schema
+
+  object :todo_item do
+    field :id, non_null(:id) # ID!
+    field :content, non_null(:string) # String!
+    field :is_completed, non_null(:boolean) do
+      resolve(fn %{completed_at: completed_at}, _, _ ->
+        {:ok, !is_nil(completed_at)}
+      end)
+    end
+  end
+
+  query do
+    field :hello, :string do
+      resolve fn _, _ ->
+        {:ok, "Hello World"}
+      end
+    end
+
+    field :todo_items, non_null(list_of(:todo_item)) do # [TodoItem!]!
+      resolve fn _, _ ->
+        {:ok, TodoList.Todos.list_items()}
+      end
+    end
+  end
+end
